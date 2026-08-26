7968ff21dbf6fb522426f1e8212023f7b7c345e9
Kushal <kushalrsg@gmail.com>
2026-08-26 23:15:28 +0530

    add ifElse practice questions

--- ifElse/q12.java
+++ ifElse/q12.java
@@ -3,5 +3,7 @@ package ifElse;
 public class q12 {
     //given a point (x,y) write a program to find if it lies in the 1st quadrant,
     //2nd quadrant, 3rd quadrant , 4th quadrant on the x-axis, y-axis or the origin
+    public static void main(String[] args) {
 
+    }
 }
--- ifElse/q13.java
+++ ifElse/q13.java
@@ -16,6 +16,6 @@ public class q13 {
 
         if (a>=b && a>=c) System.out.println(a);
         else if (b>=a && b>=c) System.out.println(b);
-        else if (c>=a && c>=b) System.out.println(c);
+        else System.out.println(c);
     }
 }
